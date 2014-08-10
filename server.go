21402460b246832b5428e3f6186810ef33d4fcb4
Mark Berger <mjberger@fb.com>
2014-08-10 12:43:08 -0700

    Registers files in vendor with http.Handle

--- server.go
+++ server.go
@@ -8,10 +8,28 @@ import (
 	"net/http"
 )
 
+// Maps files in path to be served at the given url
+
+func registerDir(url string, path string) {
+	http.Handle(
+		url,
+		http.StripPrefix(
+			url,
+			http.FileServer(http.Dir(path)),
+		),
+	)
+}
+
+func registerVendor() {
+	registerDir("/static/bootstrap/", "./vendor/bootstrap-3.2.0/")
+	registerDir("/static/dropzone/", "./vendor/dropzone-3.10.2/")
+}
+
 func main() {
 	b, _ := db.NewBoltManager("./bolt.db")
 	jar := sessions.NewCookieStore([]byte("secret key"))
 	api.RegisterHandlers(b, jar)
+	registerVendor()
 
 	// ClearHandler is for gorilla/sessions. There will be a
 	// memory leak without it
