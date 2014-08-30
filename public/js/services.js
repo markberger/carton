b104293b58d20bf2d84cff416ec7ad14c071e540
Mark J. Berger <mark.berger.j@gmail.com>
2014-08-30 00:10:59 -0700

    Fixes UserService

--- public/js/services.js
+++ public/js/services.js
@@ -6,23 +6,13 @@
         .service('UserService', ['$http',
 			function($http) {
 
-				var sdo = {
-					isLogged: false
-				}
+				var userSrv = this;
+				userSrv.isLogged = false;
 
 				$http({method: 'GET', url: '/api/auth/status'})
 
 				.success(function(data, status, headers, config) {
-					if (data.status) {
-						sdo.isLogged = true;
-							return sdo;
-					} else {
-						return sdo;
-					}
-				})
-
-				.error(function(data, status, headers, config) {
-					return sdo;
+					userSrv.isLogged = true;
 				});
         }]);
 })();
